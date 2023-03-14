88df25e39c6ee6e25cad76887f592294bf6db580
nagsides <nags.ides@gmail.com>
2023-03-14 19:15:23 +0530

    Docs added to WeatherForecastControllers.cs and Programs.cs files

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,3 +1,47 @@
+/*
+This code is an example of a simple ASP.NET Core Web API controller that returns a list of 
+weather forecasts. Here is an explanation of the various parts of the code:
+
+The first line of the code imports the Microsoft.AspNetCore.Mvc namespace, which provides 
+the necessary classes and interfaces for building web applications using ASP.NET Core.
+
+The next few lines define the namespace and class for the controller. 
+The [ApiController] attribute is used to indicate that this controller should automatically 
+infer the binding source parameters from the request's properties such as the query string, 
+request body, and route data. 
+The [Route("[controller]")] attribute specifies the base route of the controller. 
+In this case, the route will be the name of the controller, which is "WeatherForecast".
+
+The class contains a static array of strings called Summaries that stores various weather 
+conditions.
+
+The constructor takes an ILogger<WeatherForecastController> instance as an input parameter, 
+which is used to log any exceptions or informational messages that might arise during the 
+execution of the code.
+
+The [HttpGet(Name = "GetWeatherForecast")] attribute is used to indicate that the 
+Get method should respond to HTTP GET requests. 
+The "Name" property of the attribute is used to give the action a unique name that can be 
+used to generate a URL for the action.
+
+The Get method returns an IEnumerable<WeatherForecast>, which is an array of objects 
+containing the date, temperature, and summary for each forecast. 
+The method uses the LINQ extension method Enumerable.Range to generate a sequence of 
+integers from 1 to 5, and then uses the Select method to project each integer in the 
+sequence into a new WeatherForecast object. 
+The WeatherForecast object's properties are set using random values generated using the 
+Random class's Shared property. The Date property is set using the DateOnly struct's 
+FromDateTime method, which returns a new instance of the struct containing only the 
+date portion of the DateTime value returned by DateTime.Now.AddDays(index). 
+Finally, the ToArray method is used to convert the sequence of WeatherForecast objects 
+into an array.
+
+Overall, this code demonstrates the basics of building a simple web API using ASP.NET Core. 
+It defines a controller with a single action that returns a collection of objects, 
+and uses a few built-in ASP.NET Core features such as attribute routing and logging.
+*/
+
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyMicroservice.Controllers;
--- Program.cs
+++ Program.cs
@@ -1,3 +1,38 @@
+/*
+This code is an example of configuring a simple ASP.NET Core web application with 
+Swagger documentation. Here is an explanation of the various parts of the code:
+
+The first line of the code creates a new instance of the WebApplication builder class, 
+which provides a fluent API for configuring the application.
+
+The next few lines configure the services that are registered with the application's 
+dependency injection container. 
+The first line adds the controllers to the container, which are used to handle incoming 
+HTTP requests. 
+The next two lines configure Swagger documentation. AddEndpointsApiExplorer() adds an 
+endpoint that serves the Swagger JSON document, and AddSwaggerGen() generates the Swagger 
+specification based on the attributes of the application's controllers and models.
+
+The next line builds the application based on the configuration specified in the builder.
+
+The next block of code sets up the middleware pipeline that will handle incoming HTTP 
+requests. If the application is running in development mode (which is determined by 
+checking the environment variable ASPNETCORE_ENVIRONMENT), the app.UseSwagger() and app.
+UseSwaggerUI() middleware are added to the pipeline to serve the Swagger documentation. 
+The UseAuthorization middleware is added to enable authorization checks on incoming 
+requests. 
+The app.MapControllers() method maps the incoming requests to the appropriate controller 
+action, based on the route defined in the controller.
+
+Finally, the app.Run() method is called to start the application and begin listening for 
+incoming HTTP requests.
+
+Overall, this code demonstrates the basics of configuring a simple web application 
+using ASP.NET Core, including adding controllers and middleware, and configuring Swagger 
+documentation.
+
+*/
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
